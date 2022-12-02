01ffa6ecd5318d5fd0cbc26535399a31009e9f52
chaney <cy0804bamboo@naver.com>
2022-12-02 17:30:40 +0900

    python 파일 생성

---
+++ Docs.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from time import sleep
+st.set_page_config(
+    page_icon="🍓",
+    page_title="KOREATECH 2022-2 Cloud Computing",
+    layout="wide",
+)
+
+st.subheader("DOCUMENT")
